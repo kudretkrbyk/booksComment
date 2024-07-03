0851f9a04d7c2a49075a9ba62559e8413eda61ff
kudretkrbyk <kudretkrbyk@gmail.com>
2024-07-03 11:32:11 +0300

    pages added

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="flex items-center justify-center gap-2">
+      <div>Kitaplar</div>
+      <div>Kitaplardan Cümleler</div>
+      <div>Okurlar</div>
+    </div>
+  );
+}
---
+++ client/src/pages/Books.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Books() {
+  return (
+    <div>
+      <div>kitap foto</div>
+      <div>
+        {" "}
+        <div>kitap adı</div>
+        <div>Yazar Adı</div>
+        <div>Yorum Sayısı</div>
+      </div>
+      <div>
+        <div>Okuma listesine kaydet</div>
+        <div>Yorumları gör</div>
+      </div>
+    </div>
+  );
+}
