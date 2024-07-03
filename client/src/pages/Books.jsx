fe6172af6921647224fbf3e0c35276a8347aea75
kudretkrbyk <kudretkrbyk@gmail.com>
2024-07-03 23:28:14 +0300

    books page style added

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -18,18 +18,32 @@ export default function Books() {
   return (
     <div className="flex flex-col  gap-5">
       {books.map((book) => (
-        <div key={book.id} className="flex items-center justify-start gap-10">
+        <div
+          key={book.id}
+          className="flex items-center justify-start gap-10 shadow-xl p-10"
+        >
           <div>
-            <img className="w-32 h-48" src={book.bookphoto}></img>
+            <img
+              className="w-32 h-48 hover:scale-110 duration-700"
+              src={book.bookphoto}
+            ></img>
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col items-start justify-center gap-5">
             {" "}
             <div className="font-bold text-xl"> {book.bookname} </div>
-            <div> {book.writername} </div>
-          </div>
-          <div>
-            <div>Okuma listesine kaydet</div>
-            <div>Yorumları gör</div>
+            <div> {book.writername} </div>{" "}
+            <div className="flex  items-center justify-center gap-4 text-white">
+              <div>
+                <button className="bg-gray-400 p-2 px-4 rounded-2xl">
+                  Okuma listesine kaydet
+                </button>
+              </div>
+              <div>
+                <button className="bg-gray-400 p-2 px-4 rounded-2xl">
+                  Yorumları gör
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       ))}
